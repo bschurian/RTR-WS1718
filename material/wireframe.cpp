00934dc4a271e8e9d8e04804d6d379ca580a2585
s74588 <s74588@beuth-hochschule.de>
2017-11-19 22:39:06 +0100

    textures

--- material/wireframe.cpp
+++ material/wireframe.cpp
@@ -5,6 +5,6 @@ void WireframeMaterial::apply(unsigned int)
     prog_->bind();
     prog_->setUniformValue("wireframeColor", wireframeColor);
 
-    qDebug() << "applying wireframe material";
+    //qDebug() << "applying wireframe material";
 }
 
